17d17cbf2e48ae30bec65d86a0d40056ff814b71
Thiago Nepomuceno <thi.nepo@gmail.com>
2022-06-30 16:03:32 -0300

    feat: updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git commit -m 'deploy'
 
 git branch -M gh-pages
 
-git remote add origin https://github.com/<USERNAME>/<REPO>.git
+git remote add origin https://github.com/NepsAcadmy/curso-frontend-vue.git
 
 git push -u -f origin gh-pages
 
